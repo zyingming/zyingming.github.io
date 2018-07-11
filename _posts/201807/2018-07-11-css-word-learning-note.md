3e379a5ff7f7d60d2cd975a54f9c4764210b2954
张英明 <2570332082@qq.com>
2018-07-11 14:19:09 +0800

    change post icon

---
+++ _posts/201807/2018-07-11-css-word-learning-note.md
@@ -0,0 +1,10 @@
+---
+layout: post
+title:  "default title"
+date:   2018-07-11 14:16:44
+categories: javascript
+tags: default tags
+icon: translate
+author: "zyingming"
+---
+## 测试翻译
--- write-head-tool.js
+++ write-head-tool.js
@@ -11,6 +11,7 @@ let w_data = '---\r\n'+
 'date:   '+ moment(Date.now()).format('YYYY-MM-DD HH:mm:ss') +'\r\n'+
 'categories: javascript\r\n'+
 'tags: default tags\r\n'+
+'icon: original\r\n'+
 'author: "zyingming"\r\n'+
 '---';
 
