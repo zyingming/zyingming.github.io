741ef5ff1f971c6577756b29a7877185be94871e
张英明 <2570332082@qq.com>
2018-07-13 11:48:55 +0800

    回滚到顶部

---
+++ assets/js/post-detail.js
@@ -0,0 +1,22 @@
+$(function() {
+	var init = function() {
+		var upperLimit = 100;
+
+		var scrollElem = $('.top-link-wrapper');
+		scrollElem.hide();
+
+		$(window).on('scroll', function() {
+			var scrollTop = $(document).scrollTop();
+			if ( scrollTop > upperLimit ) {
+	      		$(scrollElem).stop().fadeTo(300, 1);           
+	    	}else{       
+	      		$(scrollElem).stop().fadeTo(300, 0); 
+	    	}
+		})
+		$('#topLink').click(function(){
+
+	    	$('html, body').animate({scrollTop:0}, 500); return false;
+	  	});
+	}
+	init();
+})
